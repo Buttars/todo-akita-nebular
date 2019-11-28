bccec8b2c862c27d34f37f27d6e25124e2580f76
Landon Buttars <landon.buttars@wgu.edu>
2019-11-27 21:12:50 -0700

    refactor: make everything look pretty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,22 @@ import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, environment.production ? [] : AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule.forRoot(),
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { NbButtonModule, NbListModule, NbInputModule, NbCheckboxModule, NbCardModule, NbIconModule } from '@nebular/theme';
+
 import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './todos/todos.component';
 import { TodosContainer } from './todos/todos.container';
@@ -9,6 +11,6 @@ import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [TodosComponent, TodosContainer, TodoComponent],
-  imports: [CommonModule, TodosRoutingModule, ReactiveFormsModule]
+  imports: [CommonModule, TodosRoutingModule, ReactiveFormsModule, NbButtonModule, NbListModule, NbInputModule, NbCheckboxModule, NbIconModule, NbCardModule]
 })
 export class TodosModule {}
